9c050ff218dc231538d7174480eb93894a62366f
bear3z <46402041+bear3z@users.noreply.github.com>
2019-01-07 21:49:59 +0800

    Update IWantDataPlan.py

--- IWantDataPlan.py
+++ IWantDataPlan.py
@@ -3,11 +3,10 @@ from bs4 import BeautifulSoup
 
 url  = "http://top100.nchu.edu.tw/nchubody2.php"
 res  = requests.get(url)
-
-list = ['排名：', '單位：', 'IP：', '上傳：', '下載：', '總流量：']
+soup = BeautifulSoup(res.text, 'html.parser')
 
 n = 0
-soup = BeautifulSoup(res.text, 'html.parser')
+list = ['排名：', '單位：', 'IP：', '上傳：', '下載：', '總流量：']
 for font in soup.select('[color="red"]'):
 	print( list[n] + font.get_text() )
 	n = n+1
